91fe5f6615ea13e7b799b8ee894df979d4394a93
Henri Cook <henri.cook@digital.hmrc.gov.uk>
2016-08-25 16:05:25 +0100

    Porting fasttrack data generator changes for roles to faststream

--- test/connectors/AuthProviderClientSpec.scala
+++ test/connectors/AuthProviderClientSpec.scala
@@ -16,10 +16,24 @@
 
 package connectors
 
+import config.WSHttp
+import connectors.ExchangeObjects.Implicits._
+import connectors.ExchangeObjects._
+import mockws.MockWS
+import model.Exceptions.ConnectorException
+import org.mockito.Matchers._
+import org.mockito.Mockito._
 import org.scalatest.concurrent.ScalaFutures
 import org.scalatest.mock.MockitoSugar
 import org.scalatestplus.play.PlaySpec
+import play.api.libs.json.{Json, _}
+import play.api.mvc.Action
+import play.api.mvc.Results._
+import play.api.test.Helpers._
 import testkit.ShortTimeout
+import uk.gov.hmrc.play.http.{HeaderCarrier, HttpResponse, _}
+
+import scala.concurrent.Future
 
 class AuthProviderClientSpec extends PlaySpec with MockitoSugar with ScalaFutures with ShortTimeout {
 
